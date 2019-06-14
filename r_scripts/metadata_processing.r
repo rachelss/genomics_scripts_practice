68952bcfd2edf4c4a69e26fc32cf233a716091bf
Rachel Schwartz <rsschwartz@gmail.com>
2019-06-14 15:18:55 +0000

    add something to delete

--- r_scripts/metadata_processing.r
+++ r_scripts/metadata_processing.r
@@ -4,3 +4,5 @@ library(dplyr)
 #look at Ecoli metadata - count number of cit+ individuals
 ecoli <- read.csv("data_info/Ecoli_metadata_composite.csv")
 ecoli %>% count(cit)
+
+#add something that doesn't work

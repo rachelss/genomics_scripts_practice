3d92050e55cac58d233154d617048d54a3f575ff
Rachel Schwartz <rsschwartz@gmail.com>
2019-06-14 14:40:34 +0000

    remove extra lines in processing script

--- r_scripts/metadata_processing.r
+++ r_scripts/metadata_processing.r
@@ -1,9 +1,6 @@
-#for loop through each file
-#get rid of extension using basename
-#move file (current file name = $f) to output from above followed by _2019.txt
-
-install.packages("dplyr")
+#install.packages("dplyr")
 library(dplyr)
-#look at Ecoli metadata
+
+#look at Ecoli metadata - count number of cit+ individuals
 ecoli <- read.csv("data_info/Ecoli_metadata_composite.csv")
 ecoli %>% count(cit)
